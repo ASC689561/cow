c8b05089b1086668d51c18b4d2c3b7c72136f627
nbhoa <asc686f61@gmail.com>
2018-02-23 11:17:03 +0700

    Thêm visitor pattern

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='cow',
     version='1.0.0',
-    packages=['cow', 'cow.patterns','cow.patterns.observer','cow.patterns.singleton'],
+    packages=['cow', 'cow.patterns','cow.patterns.observer','cow.patterns.singleton','cow.patterns.visitor'],
     zip_safe=False,
     install_requires=[]
 )
