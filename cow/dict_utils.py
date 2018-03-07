cebdc6593d021d24576a0696598124f926f2a23a
nbhoa <asc686f61@gmail.com>
2018-03-07 15:40:42 +0700

    Theem update dict

--- cow/dict_utils.py
+++ cow/dict_utils.py
@@ -4,3 +4,19 @@ def get_md5(dic):
 
     data_md5 = hashlib.md5(json.dumps(dic, sort_keys=True).encode()).hexdigest()
     return str(data_md5)
+
+
+def update(dic1, dic2):
+    for v in dic1.keys():
+        if v in dic2:
+            dic1[v] = dic2[v]
+
+
+def remove_key_except(dic, keys):
+    remove_keys=[]
+    for v in dic:
+        if v not in keys:
+            remove_keys.append(v)
+    for v in remove_keys:
+        dic.pop(v)
+
