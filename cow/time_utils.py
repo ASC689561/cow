10a2fef1feaec9eede62db3d2ee43d79b459c6e7
asc686f61 <asc689561@yahoo.com>
2018-04-01 00:05:37 +0700

    Thêm get ntp time

---
+++ cow/time_utils.py
@@ -0,0 +1,30 @@
+import datetime
+
+import ntplib
+import pytz
+
+import logging
+
+init_time = {}
+
+
+def get_ntp_time(time_zone='Asia/Ho_Chi_Minh'):
+    local_time_zone = pytz.timezone("Asia/Ho_Chi_Minh")  # time zone name from Olson database
+
+    def get_time_from__ntp_client():
+        logging.debug('Get time from ntp server')
+        c = ntplib.NTPClient()
+        response = c.request('pool.ntp.org', version=3)
+        formatted_date_with_micro_seconds = datetime.datetime.strptime(
+            str(datetime.datetime.utcfromtimestamp(response.tx_time)), "%Y-%m-%d %H:%M:%S.%f")
+        local_dt = formatted_date_with_micro_seconds.replace(tzinfo=pytz.utc).astimezone(local_time_zone)
+        formatted_date_with_corrections = str(local_dt).split("+")[0]
+        return datetime.datetime.strptime(formatted_date_with_corrections, "%Y-%m-%d %H:%M:%S.%f")
+
+    if time_zone not in init_time:
+        time = get_time_from__ntp_client()
+        init_time[time_zone] = (datetime.datetime.now(), time)
+    time_local, time_internet = init_time[time_zone]
+    now = time_internet + datetime.timedelta(seconds=(datetime.datetime.now() - time_local).total_seconds())
+
+    return now
