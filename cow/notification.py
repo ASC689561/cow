77eab71bb428c8081d1ca76f7ccf839aead38535
asc686f61 <asc689561@yahoo.com>
2018-04-01 04:35:43 +0700

    thêm notify message

--- cow/notification.py
+++ cow/notification.py
@@ -1,6 +1,6 @@
 import subprocess
 
 
-def notify_message(message):
-    subprocess.Popen(['notify-send', message])
+def notify_message(message, level='normal'):
+    subprocess.Popen(['notify-send', '-u', level, message])
     return
