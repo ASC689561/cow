815e45b945206f0f360acf8757ad8000da7b906f
nbhoa <asc686f61@gmail.com>
2018-04-17 14:28:12 +0700

    Thêm flask util

--- cow/flask_util.py
+++ cow/flask_util.py
@@ -1,6 +1,6 @@
-import json
 import logging
 import traceback
+from functools import wraps
 
 SUCCESS = 200  # thành công
 UNKNOWN_EXCEPTION = 201  # lỗi unknown
@@ -14,18 +14,18 @@ class WebServiceException:
 
 
 def auto_try_catch(method):
+    @wraps(method)
     def try_catch(*args, **kw):
+        from flask import jsonify
+
         try:
             result = method(*args, **kw)
-            return json.dumps({'code': SUCCESS, 'status': 'success', 'data': result, 'message': None},
-                              default=lambda o: o.__dict__)
+            return jsonify({'code': SUCCESS, 'status': 'success', 'data': result, 'message': None})
         except Exception as ex:
             logging.error(traceback.format_exc())
             code = ex.__dict__.get('code', None)
             if code is not None:
-                return json.dumps({'code': code, 'status': 'error', 'data': None, 'message': str(ex)},
-                                  default=lambda o: o.__dict__)
-            return json.dumps({'code': UNKNOWN_EXCEPTION, 'status': 'error', 'data': None, 'message': str(ex)},
-                              default=lambda o: o.__dict__)
+                return jsonify({'code': code, 'status': 'error', 'data': None, 'message': str(ex)})
+            return jsonify({'code': UNKNOWN_EXCEPTION, 'status': 'error', 'data': None, 'message': str(ex)})
 
     return try_catch
