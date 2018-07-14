9667f154a6faf0a640a976d8bc3462db6fde5e09
asc686f61 <asc686f61@yahoo.com>
2018-07-14 21:49:53 +0700

    Thêm progress

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='cow',
-    version='1.0.0',
+    version='1.1.1',
     packages=['cow', 'cow.patterns','cow.patterns.observer','cow.patterns.singleton','cow.patterns.visitor'],
     zip_safe=False,
     install_requires=[]
