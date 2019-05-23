c550327259d7a2c525513546bb46f635d2422035
nbhoa <asc686f61@gmail.com>
2019-05-23 10:13:46 +0700

    them python-logstash-async

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     version='1.1.1',
     packages=['cow', 'cow.patterns','cow.patterns.observer','cow.patterns.singleton','cow.patterns.visitor'],
     zip_safe=False,
-    install_requires=['requests','rsa','ntplib','pytz']
+    install_requires=['requests','rsa','ntplib','pytz','python-logstash-async']
 )
