af8183b8c043e2b33049a9a328bd91e82ad91d61
nbhoa <asc686f61@gmail.com>
2019-07-23 11:01:31 +0700

    add is-docker

--- cow/__init__.py
+++ cow/__init__.py
@@ -3,16 +3,17 @@ import sys
 
 from .cache import create_disk_cache
 from .curl_util import execute_curl
-from .debug import is_debug
+from .debug import is_debug, is_docker
 from .dict_utils import remove_key_except, md5
 from .function_util import timing, ignore_exception
 from .lazy_load import LazyProperty
 from .logging_util import LogBuilder
 from .notification import notify_message
 from .path_util import ensure_directory_exists
+from .rsa_encrpyt_helper import RSAEncryptHelper
 from .string_util import keymap_replace
 from .time_utils import get_ntp_time
 from .webservice_util import *
-from .rsa_encrpyt_helper import RSAEncryptHelper
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/patterns/observer")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/patterns/singleton")
