34a2aa84535fd8d4bc41cd26a955b7d74645f74f
nbhoa <asc686f61@gmail.com>
2019-07-23 10:58:50 +0700

    add is-docker

--- cow/debug.py
+++ cow/debug.py
@@ -1,8 +1,13 @@
 import sys
 
 
-def is_debug()->bool:
+def is_debug() -> bool:
     if 'pydevd' in sys.modules:
         return True
     else:
         return False
+
+
+def is_docker() -> bool:
+    with open('/proc/1/cgroup', 'rt') as ifh:
+        return 'docker' in ifh.read()
