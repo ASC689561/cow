e8e34b6e972079f370faca64653898e559c69413
asc686f61 <asc686f61@yahoo.com>
2019-08-21 21:03:25 +0700

    sua import

--- setup.py
+++ setup.py
@@ -3,7 +3,12 @@ from setuptools import setup
 setup(
     name='cow',
     version='1.1.1',
-    packages=['cow', 'cow.patterns','cow.patterns.observer','cow.patterns.singleton','cow.patterns.visitor'],
+    packages=['cow',
+              'cow.patterns',
+              'cow.patterns.service_resgistry',
+              'cow.patterns.observer',
+              'cow.patterns.singleton',
+              'cow.patterns.visitor'],
     zip_safe=False,
-    install_requires=['requests','rsa','ntplib','pytz','python-logstash-async']
+    install_requires=['requests', 'rsa', 'ntplib', 'pytz', 'python-logstash-async']
 )
