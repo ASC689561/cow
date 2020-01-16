2af3d8eb07778fadf64157264798e8a9174e38ee
nbhoa <asc686f61@gmail.com>
2020-01-16 15:40:52 +0700

    thêm set set_max_width_

--- cow/streamlitutils.py
+++ cow/streamlitutils.py
@@ -11,6 +11,20 @@ class Page:
         pass
 
 
+def set_max_width():
+    max_width_str = f"max-width: 2000px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
+
+
 def run():
     import streamlit as st
     all_class = {x.name: x for x in get_subclasses(Page)}
