677d3b80d45826ea127d553d4715387a9e333532
nbhoa <asc686f61@gmail.com>
2020-02-22 09:54:34 +0700

    release v6.8.9 thêm timer

--- cow/__init__.py
+++ cow/__init__.py
@@ -10,11 +10,11 @@ from .logging_util import LogBuilder
 from .notification import notify_message
 from .rsa_encrpyt_helper import RSAEncryptHelper
 from .string_util import keymap_replace
-from .time_utils import get_ntp_time,Timer
+from .time_utils import get_ntp_time
 from .webservice_util import *
 from .class_util import get_subclasses
 from .streamlitutils import run,Page,set_max_width
-
+from .timer import Timer
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/patterns/observer")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/patterns/visitor")
