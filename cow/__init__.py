96ed8c6f77fa4f12b066710a82efb3f1b3e396e3
asc686f61 <asc686f61@yahoo.com>
2020-09-04 22:28:41 +0700

    update streamlit run

--- cow/__init__.py
+++ cow/__init__.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from .text_generate import generate_permutation
+from .class_util import get_subclasses
 from .curl_util import execute_curl
 from .debug import is_debug, is_docker
 from .function_util import timing, ignore_exception
@@ -9,12 +9,10 @@ from .lazy_load import LazyProperty
 from .notification import notify_message
 from .rsa_encrpyt_helper import RSAEncryptHelper
 from .string_util import keymap_replace
+from .text_generate import generate_permutation
 from .time_utils import get_ntp_time
-from .webservice_util import *
-from .class_util import get_subclasses
 from .timer import Timer
+from .webservice_util import *
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/patterns/observer")
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/patterns/visitor")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/streamlit_")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/logging_")
--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ setup(
     name='cow',
     version='1.1.1',
     packages=['cow',
-              'cow.patterns',
-              'cow.patterns.observer',
-              'cow.patterns.visitor',
               'cow.streamlit_'
+              'cow.logging_'
               ],
     zip_safe=False,
     install_requires=['yaml', 'requests', 'rsa', 'ntplib==0.3.4', 'pytz', "colorlog"]
