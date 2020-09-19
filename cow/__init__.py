4ef13744fa98adc6999f3fb7bb654f31aa4631c1
asc686f61 <asc686f61@yahoo.com>
2020-09-19 16:48:04 +0700

    them ignore_exception

--- cow/__init__.py
+++ cow/__init__.py
@@ -13,6 +13,7 @@ from .text_generate import generate_permutation
 from .time_utils import get_ntp_time
 from .timer import Timer
 from .webservice_util import *
+from .utils import ignore_exception
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/streamlit_")
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/logging_")
