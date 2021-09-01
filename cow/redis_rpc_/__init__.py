f546e317734fbf502300511a7d274ef6d0ad127b
asc686f61 <asc686f61@yahoo.com>
2021-09-01 09:24:47 +0700

    fix rpc

--- cow/redis_rpc_/__init__.py
+++ cow/redis_rpc_/__init__.py
@@ -1,18 +1,41 @@
-from .redis_rpc import Client, Server
+from .redis_rpc__ import Client,Server
 
 """
+-- server
+import redis
+from cow.redis_rpc_ import Server
 
---- server 
-server = Server('/tmp/rpc',redis_url='redis://chatbottest.misa.com.vn:90')
- 
-@server.method
-def add(a, b):
-    print('xx')
-    return a + b
 
+class Calculator(object):
+
+    def __init__(self):
+        self.acc = 0.0
+
+    def __str__(self):
+        return '%s' % self.acc
+
+    def add(self, number):
+        self.acc += number
+        return self.acc
+
+
+redis_server = redis.Redis()
+message_queue = 'calc'
+local_object = Calculator()
+server = Server(redis_server, message_queue, local_object)
 server.run()
 
---- client 
-client = Client('/tmp/rpc',redis_url='redis://chatbottest.misa.com.vn:90')
-print(client.add(1, 2))
+
+-- client
+
+import redis 
+from cow.redis_rpc_ import Server,Client
+
+
+redis_server = redis.Redis()
+message_queue = 'calc'
+calculator = Client(redis_server, message_queue, timeout=1, transport='pickle')
+
+print('success!',calculator.add(1))
+
 """
